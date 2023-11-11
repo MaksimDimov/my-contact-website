36fcf98b2cfca41d4aa476ff46196de8a4383c40
MaksimDimov <maksymdimov@student.mitt.ca>
2023-11-11 07:10:14 +0200

    commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const sectionNavigator = document.querySelectorAll('a[href^="#"]');
+const links = document.querySelectorAll('a[href^="#"]');
 
-document.addEventListener('DOMContentLoaded', function () {
-    sectionNavigator.forEach(link => {
+links.addEventListener('DOMContentLoaded', function () {
+    links.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
+            const targetId = this.getAttribute('href');
             const targetElement = document.getElementById(targetId);
             if (targetElement) {
                 targetElement.scrollIntoView({
                     behavior: 'smooth'
-            
                 });
             }
         });
